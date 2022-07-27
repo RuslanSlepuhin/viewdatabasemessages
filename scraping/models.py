cef73e3c3b7dbbda2824a1c5af7daf1690295942
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-07-27 22:25:18 +0300

    only get

--- scraping/models.py
+++ scraping/models.py
@@ -19,7 +19,7 @@ class Telegram(models.Model):
 
 class NewYork(models.Model):
     chat_name = models.CharField(max_length=150, blank=True, null=True)
-    title = models.CharField(max_length=700, blank=True, null=True)
+    title = models.CharField(max_length=1000, blank=True, null=True)
     body = models.CharField(max_length=6000, blank=True, null=True)
     time_of_public = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
--- scraping/urls.py
+++ scraping/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import GetAllMessagesView, CreateMessageView
+from .views import GetAllMessagesView
 
 urlpatterns = [
     path('messages/', GetAllMessagesView.as_view({'get': 'list'})),
-    path('mmm/', CreateMessageView.as_view()),
 ]
